50f9b41d14b1d82cfe731880fa3199b988903305
kevin <kevin@hinshawfam.com>
2020-01-16 05:49:18 -0600

    adds new control statements examples

---
+++ control-statements/DoWhileTests.java
@@ -0,0 +1,13 @@
+//do...while repetition statement
+public class DoWhileTests {
+
+    public static void main(String[] args) {
+        int counter = 1;
+        do {
+            System.out.printf("%d ", counter);
+            ++counter;
+        } while (counter <= 10);
+
+        System.out.println();
+    }
+}
