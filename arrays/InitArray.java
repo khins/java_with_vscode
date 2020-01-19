b7d25323178ed1642ca2f1b57dd5274ebc529b84
kevin <kevin@hinshawfam.com>
2020-01-19 06:08:39 -0600

    Calculating the values to be placed into the elements of an array

--- arrays/InitArray.java
+++ arrays/InitArray.java
@@ -1,8 +1,13 @@
-// Initializing the elements of an array with an array initializer
+// Calculating the values to be placed into the elements of an array
 public class InitArray {
 
     public static void main(String[] args) {
-        int[] array = {1,2,3,4,5,6,7,8,9,10};
+        final int Array_Length = 100;
+        int[] array = new int[Array_Length];
+        
+        for (int i = 0; i < array.length; i++) {
+            array[i] = 2+2*i;
+        }
         System.out.printf("%s%8s%n", "index", "value");
         for (int i = 0; i < array.length; i++) {
             System.out.printf("%5d%8d%n", i, array[i]);
