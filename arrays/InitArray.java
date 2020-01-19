24a7d036fb21c45aa5e5dc51f86db2c56a36b063
kevin <kevin@hinshawfam.com>
2020-01-19 06:02:04 -0600

    Initializing the elements of an array with an array initializer

--- arrays/InitArray.java
+++ arrays/InitArray.java
@@ -1,8 +1,8 @@
-// Initializing the elements of an array to default values of zero
+// Initializing the elements of an array with an array initializer
 public class InitArray {
 
     public static void main(String[] args) {
-        int[] array = new int[10];
+        int[] array = {1,2,3,4,5,6,7,8,9,10};
         System.out.printf("%s%8s%n", "index", "value");
         for (int i = 0; i < array.length; i++) {
             System.out.printf("%5d%8d%n", i, array[i]);
