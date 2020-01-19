8435146fe98181015a05533ddf16fc35e8a315d0
kevin <kevin@hinshawfam.com>
2020-01-19 08:25:16 -0600

    Demo: Packages

---
+++ ps-examples/com/hinshawfam/Organized/Organized.java
@@ -0,0 +1,9 @@
+package com.hinshawfam.Organized;
+// Demo: Packages
+// https://app.pluralsight.com/course-player?clipId=da2310ef-e2f5-4ca8-b27d-6cc005e8b4cf
+public class Organized {
+    public static void main(String[] args) {
+        System.out.println("In class Organized");
+    }
+    
+}
