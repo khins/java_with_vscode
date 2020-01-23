a5d104d7149c8566089192b234b1eed683ee742d
kevin <kevin@hinshawfam.com>
2020-01-23 05:58:46 -0600

    update

--- objects-classes/Time1.java
+++ objects-classes/Time1.java
@@ -1,6 +1,20 @@
 /**
  * Time1 class declaration maintains the time in 24-hour format
  * video https://learning.oreilly.com/videos/java-8-fundamentals/9780133489354/9780133489354-JFUN_lesson08_01
+ * c:\Users\kevin\Documents\Learning Library\Java\java_with_vscode\bin>java -classpath "C:\Users\kevin\Documents\Learning Library\Java\java_with_vscode\bin" Time1Test
+After object is created
+Universal time: 00:0000
+Standard time: 12:0000 AM
+
+After calling setTime
+Universal time: 13:2706
+Standard time: 01:2706 PM
+
+Exception: value out of range
+
+Afer calling setTime with invalid args
+Universal time: 13:2706
+Standard time: 01:2706 PM
  */
 public class Time1 {
 
