0b4f38ab4d1db9338e835dc2a02072b72543f9df
kevin <kevin@hinshawfam.com>
2020-01-25 06:28:35 -0600

    static import of Math class methods

---
+++ objects-classes/StaticImportTest.java
@@ -0,0 +1,13 @@
+import static java.lang.Math.*;
+/**
+ * static import of Math class methods
+ * video https://learning.oreilly.com/videos/java-8-fundamentals/9780133489354/9780133489354-JFUN_lesson08_09
+ */
+public class StaticImportTest {
+
+    public static void main(final String[] args) {
+        System.out.printf("sqrt(900) = %.1f%n", sqrt(900.0)); // bad programming practices
+        System.out.printf("ceil(-9.8) = %.1f%n", ceil(-9.8));
+
+    }
+}
